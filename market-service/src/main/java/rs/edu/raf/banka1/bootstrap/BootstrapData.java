80bccbe130507f25b30fbdf75a180ba3d8d35385
Kovelja009 <vanjakovinic009@gmail.com>
2024-03-11 22:08:47 +0100

    added ListingModel and ListingHistoryModel

--- market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import rs.edu.raf.banka1.mapper.ListingMapper;
 import rs.edu.raf.banka1.model.ListingHistoryModel;
 import rs.edu.raf.banka1.model.ListingModel;
+import rs.edu.raf.banka1.repositories.ListingRepository;
 import rs.edu.raf.banka1.services.ListingService;
 
 import java.util.List;
@@ -23,22 +24,11 @@ public class BootstrapData implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         System.out.println("Loading Data...");
-
-//        fetchiing and bootstrapping listing data
-
-//        call it only from time to time to update json because api isn't free and we need it only once
-//        listingService.initializeListings();
-//        loading data from json and fetches data from other API (professor will give us API token for this one)
-        List<ListingModel> listingModels = listingService.fetchListings();
-//        use this to update or initialize database with fresh data
-        listingService.updateAllListingsDatabase(listingModels);
-
-//        fetching and bootstrapping listing history data (not recommended as each listing generates 100 history records and we have around 3500 listings)
-//        List<ListingHistoryModel> listingHistoryModels = listingService.fetchAllListingsHistory();
-//        so better alternative is to fetch history only for given listing when needed
-        List<ListingHistoryModel> oneListingHistoryList = listingService.fetchSingleListingHistory("AAPL");
-        listingService.addAllListingsToHistory(oneListingHistoryList);
-
-      System.out.println("Data loaded!");
+        System.out.println(listingService.fetchListings().size());
+        listingService.updateAllListings(listingService.fetchListings());
+        List<ListingHistoryModel> lst = List.of(listingMapper.listingModelToListongHistoryModel(listingService.fetchListings().get(0)), listingMapper.listingModelToListongHistoryModel(listingService.fetchListings().get(0)));
+        int res = listingService.addAllListingsToHistory(lst);
+        System.out.println(res + " new listings added to history!");
+        System.out.println("Data loaded!");
     }
 }
--- market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
+++ market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
@@ -6,21 +6,8 @@ import rs.edu.raf.banka1.model.ListingModel;
 import java.util.List;
 
 public interface ListingService {
-//    only do it once and store it into json file (because api isn't free and we need it once anyway)
-    void initializeListings();
-
-//    fetch all listings based on json file
     List<ListingModel> fetchListings();
-
-//    updated listing in the database
-    void updateAllListingsDatabase(List<ListingModel> listings);
-
-//    fetch all listings-history based on json file
-    List<ListingHistoryModel> fetchAllListingsHistory();
-
-//    fetch single listing-history
-    List<ListingHistoryModel> fetchSingleListingHistory(String ticker);
-
+    void updateAllListings(List<ListingModel> listings);
 //    return 1 if it's a new listing, 0 if it's just an update
     int addListingToHistory(ListingHistoryModel listingHistoryModel);
 
