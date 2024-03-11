3cb12a7117f18dd3448b9382edcf59165bc03acc
Kovelja009 <vanjakovinic009@gmail.com>
2024-03-12 00:09:31 +0100

    added fetching ListingModel from the api, apiToken added to the constants

--- market-service/src/main/java/rs/edu/raf/banka1/utils/Constants.java
+++ market-service/src/main/java/rs/edu/raf/banka1/utils/Constants.java
@@ -1,9 +1,5 @@
 package rs.edu.raf.banka1.utils;
 
-import java.io.File;
-import java.util.List;
-
 public class Constants {
-    public static String listingsFilePath = "./market-service/src/main/resources/listings.json";
-    public static List<String> sectors = List.of("Technology");
+    public static String listingAPItoken = "pk_f87286e075c94cc484405da70691c030";
 }
