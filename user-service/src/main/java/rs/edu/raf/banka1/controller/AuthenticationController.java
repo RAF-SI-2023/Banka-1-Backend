adb1208006a80c71238652ebb983561a4dfd53bd
jovanar28 <jovanar28@gmail.com>
2024-03-11 01:17:13 +0100

    Added authController

---
+++ user-service/src/main/java/rs/edu/raf/banka1/controller/AuthenticationController.java
@@ -0,0 +1,60 @@
+package rs.edu.raf.banka1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.edu.raf.banka1.model.Permission;
+import rs.edu.raf.banka1.model.User;
+import rs.edu.raf.banka1.requests.LoginRequest;
+import rs.edu.raf.banka1.responses.LoginResponse;
+import rs.edu.raf.banka1.responses.UserResponse;
+import rs.edu.raf.banka1.services.UserService;
+import rs.edu.raf.banka1.utils.JwtUtil;
+
+import java.util.stream.Collectors;
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, UserService userService, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        } catch (Exception   e){
+//            e.printStackTrace();
+            return ResponseEntity.status(401).build();
+        }
+
+        UserResponse user = this.userService.findByEmail(loginRequest.getEmail());
+        StringBuilder permissions = new StringBuilder();
+
+        if (user.getPermissions() != null && !user.getPermissions().isEmpty()) {
+            permissions.append("[");
+
+            user.getPermissions().stream().forEach((permission -> permissions.append(permission.getName()).append(", ")));
+            permissions.replace(permissions.lastIndexOf(","), permissions.length(), "");
+            permissions.append("]");
+        }
+
+
+        return ResponseEntity.ok(
+                new LoginResponse(
+                        jwtUtil.generateToken(loginRequest.getEmail(), permissions.toString()),
+                        user.getPermissions().stream().map(Permission::getName).collect(Collectors.toList())
+                )
+        );
+    }
+}
--- user-service/src/main/java/rs/edu/raf/banka1/utils/JwtUtil.java
+++ user-service/src/main/java/rs/edu/raf/banka1/utils/JwtUtil.java
@@ -2,11 +2,17 @@ package rs.edu.raf.banka1.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import rs.edu.raf.banka1.model.User;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+
 
 @Component
 public class JwtUtil {
@@ -32,6 +38,16 @@ public class JwtUtil {
         return claims.getSubject();
     }
 
+    public String generateToken(String email, String permissions){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", permissions);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
+    }
     public List<String> extractRoles(String token) {
         return null;
     }
