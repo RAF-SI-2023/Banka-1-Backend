79cf4bb13cd33b744f5951efa71301227a4c3acb
Kovelja009 <vanjakovinic009@gmail.com>
2024-03-12 21:43:52 +0100

    added fetching for ListingHistory and explained bootstrapping logic

--- market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
+++ market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
@@ -9,10 +9,17 @@ public interface ListingService {
 //    only do it once and store it into json file (because api isn't free and we need it once anyway)
     void initializeListings();
 
+//    fetch all listings based on json file
     List<ListingModel> fetchListings();
 
 //    updated listing in the database
     void updateAllListingsDatabase(List<ListingModel> listings);
+
+//    fetch all listings-history based on json file
+    List<ListingHistoryModel> fetchAllListingsHistory();
+
+//    fetch single listing-history
+    List<ListingHistoryModel> fetchSingleListingHistory(String ticker);
 
 //    return 1 if it's a new listing, 0 if it's just an update
     int addListingToHistory(ListingHistoryModel listingHistoryModel);
