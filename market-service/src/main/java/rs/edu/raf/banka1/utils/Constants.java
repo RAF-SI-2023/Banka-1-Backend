e13233de6671be1fcfc9db736444f4735177baff
Kovelja009 <vanjakovinic009@gmail.com>
2024-03-12 02:39:35 +0100

    fetching date works, but API isn't free

--- market-service/src/main/java/rs/edu/raf/banka1/utils/Constants.java
+++ market-service/src/main/java/rs/edu/raf/banka1/utils/Constants.java
@@ -1,5 +1,7 @@
 package rs.edu.raf.banka1.utils;
 
 public class Constants {
+//    after free trial ends, replace with your own token (won't be necessary if you use the json file)
     public static String listingAPItoken = "pk_f87286e075c94cc484405da70691c030";
+    public static String alphaVantageAPIToken = "B5MVJ49MXUC89AQ5";
 }
