19307d4eeaadcd4ffde920bdfc87463928b690a4
Kovelja009 <vanjakovinic009@gmail.com>
2024-03-12 02:39:35 +0100

    fetching date works, but API isn't free

--- market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
+++ market-service/src/main/java/rs/edu/raf/banka1/services/ListingService.java
@@ -6,8 +6,12 @@ import rs.edu.raf.banka1.model.ListingModel;
 import java.util.List;
 
 public interface ListingService {
+//    only do it once and store it into json file (because api isn't free and we need it once anyway)
+    void initializeListings();
+
     List<ListingModel> fetchListings();
     void updateAllListings(List<ListingModel> listings);
+
 //    return 1 if it's a new listing, 0 if it's just an update
     int addListingToHistory(ListingHistoryModel listingHistoryModel);
 
