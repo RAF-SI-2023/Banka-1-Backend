0f7dafab18435c10e85a9cd5b63581949531f714
jsimc <jelenasimic2820@gmail.com>
2024-03-12 16:23:03 +0100

    add first tests

--- user-service/src/main/java/rs/edu/raf/banka1/repositories/UserRepository.java
+++ user-service/src/main/java/rs/edu/raf/banka1/repositories/UserRepository.java
@@ -15,7 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u " +
             "where u.active = true " +
             "and (:email IS NULL or u.email like concat('%', concat(:email, '%'))) " +
-            "and (:lastName IS NULL or u.firstName like concat('%', concat(:firstName, '%'))) " +
+            "and (:firstName IS NULL or u.firstName like concat('%', concat(:firstName, '%'))) " +
             "and (:lastName IS NULL or u.lastName like concat('%', concat(:lastName, '%'))) " +
             "and (:position IS NULL or u.position like concat('%', concat(:position, '%')))")
     Optional<List<User>> searchUsersByEmailAndFirstNameAndLastNameAndPosition(@Param("email") String email,
