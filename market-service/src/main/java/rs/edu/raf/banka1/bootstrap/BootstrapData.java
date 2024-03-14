2d47aa782e7d97c2e5de902c004811a7d17468f5
laptop <ltrgovcevic5320rn@raf.rs>
2024-03-14 11:43:29 +0100

    Popravljen BootstrapData klasa, prilikom merge-a se obrisalo nekoliko linija

--- market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -1,10 +1,15 @@
 package rs.edu.raf.banka1.bootstrap;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import rs.edu.raf.banka1.mapper.ListingMapper;
+import rs.edu.raf.banka1.model.ListingHistoryModel;
+import rs.edu.raf.banka1.model.ListingModel;
 import rs.edu.raf.banka1.model.dtos.CurrencyDto;
 import rs.edu.raf.banka1.services.CurrencyService;
+import rs.edu.raf.banka1.services.ListingService;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -18,6 +23,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final CurrencyService currencyService;
 
+    @Autowired
+    private ListingService listingService;
+
+    @Autowired
+    private ListingMapper listingMapper;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -26,6 +37,21 @@ public class BootstrapData implements CommandLineRunner {
         List<CurrencyDto> currencyList = loadCurrencies();
         currencyService.addCurrencies(currencyList);
         System.out.println("Currency Data Loaded!");
+
+//        fetchiing and bootstrapping listing data
+
+//        call it only from time to time to update json because api isn't free and we need it only once
+//        listingService.initializeListings();
+//        loading data from json and fetches data from other API (professor will give us API token for this one)
+        List<ListingModel> listingModels = listingService.fetchListings();
+//        use this to update or initialize database with fresh data
+        listingService.updateAllListingsDatabase(listingModels);
+
+//        fetching and bootstrapping listing history data (not recommended as each listing generates 100 history records and we have around 3500 listings)
+//        List<ListingHistoryModel> listingHistoryModels = listingService.fetchAllListingsHistory();
+//        so better alternative is to fetch history only for given listing when needed
+        List<ListingHistoryModel> oneListingHistoryList = listingService.fetchSingleListingHistory("AAPL");
+        listingService.addAllListingsToHistory(oneListingHistoryList);
 
         System.out.println("All Data loaded!");
     }
