575a3c07668b8d94686ad2cefa68e8e6e5924bc7
Dusan Tisma <tismadusan567@gmail.com>
2024-03-17 18:53:15 +0100

    cleanup

--- market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -39,26 +39,27 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("Loading Data...");
 
         exchangeService.seedDatabase();
+        System.out.println("Exchange data loaded!");
 
-//        List<CurrencyDto> currencyList = loadCurrencies();
-//        currencyService.addCurrencies(currencyList);
-//        System.out.println("Currency Data Loaded!");
-//
-////        fetchiing and bootstrapping listing data
+        List<CurrencyDto> currencyList = loadCurrencies();
+        currencyService.addCurrencies(currencyList);
+        System.out.println("Currency Data Loaded!");
+
+//        fetchiing and bootstrapping listing data
 
 //        call it only from time to time to update json because api isn't free and we need it only once
 //        listingService.initializeListings();
 
 //        loading data from json and fetches data from other API (professor will give us API token for this one)
-//        List<ListingModel> listingModels = listingService.fetchListings();
-////        use this to update or initialize database with fresh data
-//        listingService.updateAllListingsDatabase(listingModels);
+        List<ListingModel> listingModels = listingService.fetchListings();
+//        use this to update or initialize database with fresh data
+        listingService.updateAllListingsDatabase(listingModels);
 
 //        fetching and bootstrapping listing history data (not recommended as each listing generates 100 history records and we have around 3500 listings)
 //        List<ListingHistoryModel> listingHistoryModels = listingService.fetchAllListingsHistory();
-////        so better alternative is to fetch history only for given listing when needed
-//        List<ListingHistoryModel> oneListingHistoryList = listingService.fetchSingleListingHistory("AAPL");
-//        listingService.addAllListingsToHistory(oneListingHistoryList);
+//        so better alternative is to fetch history only for given listing when needed
+        List<ListingHistoryModel> oneListingHistoryList = listingService.fetchSingleListingHistory("AAPL");
+        listingService.addAllListingsToHistory(oneListingHistoryList);
 
         System.out.println("All Data loaded!");
     }
