da7427e9458e70acce2932d40e28c36fcc259342
Vanja Kovinić <vanjakovinic009@gmail.com>
2024-03-26 22:57:40 +0100

    Refactored code to have only one BankAccount and finished my task (#134)
    * added end points for getting account by ownerId, creatorId and getting all cards by acc
    
    * added one BankAccount to unify them all
    
    * refactored end points -> tested and working
    
    * biggest delete in history -> tested, still working :)
    
    * refactored for pr review, also added Customer
    
    * Added test for BankAccount and Credit services -> all test passing
    
    * Added currency -> modifying for merge with savatar
    
    * before final merge
    
    * merged and tiny bit of refactoring -> working

--- user-service/src/main/java/rs/edu/raf/banka1/dtos/BankAccountDto.java
+++ user-service/src/main/java/rs/edu/raf/banka1/dtos/BankAccountDto.java
@@ -1,12 +1,13 @@
 package rs.edu.raf.banka1.dtos;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class BankAccountDto {
-    //    it will be assigned based on from what table it was taken
+//    it will be assigned based on from what table it was taken
     private String accountType;
     private String accountNumber;
     private String accountStatus;
--- user-service/src/main/java/rs/edu/raf/banka1/model/BankAccount.java
+++ user-service/src/main/java/rs/edu/raf/banka1/model/BankAccount.java
@@ -12,7 +12,6 @@ public class BankAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
     private String accountNumber;
@@ -26,32 +25,19 @@ public class BankAccount {
     @JoinColumn(name = "company_id", referencedColumnName = "id")
     private Company company;
 
-    @Column
     private Double balance;
-
-    @Column
     private Double availableBalance;
-
-    @Column
     private Long createdByAgentId;
-
-    @Column
     private Long creationDate;
-
-    @Column
     private Long expirationDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "currency_id", referencedColumnName = "id")
     private Currency currency;
 
-    @Column
-    private String accountStatus; //string because of frontend
+    private Boolean accountStatus;
 
-    //    Only for current_account and foreign_currency_account
-    @Column
+//    Only for current_account and foreign_currency_account
     private String subtypeOfAccount;
-
-    @Column
     private Double accountMaintenance;
 }
