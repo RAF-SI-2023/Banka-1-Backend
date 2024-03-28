9287f18a974bd8c9111faab8fa7984d7f694ca2b
Sava Ivković <80286522+savatarko@users.noreply.github.com>
2024-03-29 00:58:06 +0100

    Sprint 3 added path for creating new bank account (#170)

--- user-service/src/main/java/rs/edu/raf/banka1/controller/BankAccountController.java
+++ user-service/src/main/java/rs/edu/raf/banka1/controller/BankAccountController.java
@@ -9,15 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rs.edu.raf.banka1.dtos.BankAccountDto;
 import rs.edu.raf.banka1.dtos.CardDto;
 import rs.edu.raf.banka1.mapper.*;
 import rs.edu.raf.banka1.model.*;
+import rs.edu.raf.banka1.requests.CreateBankAccountRequest;
+import rs.edu.raf.banka1.requests.customer.AccountData;
 import rs.edu.raf.banka1.services.BankAccountService;
 import rs.edu.raf.banka1.services.CardService;
 
@@ -113,5 +111,22 @@ public class BankAccountController {
     public ResponseEntity<List<BankAccountDto>> getBankAccountsByCreatorId(@PathVariable(name = "userId") Long userId) {
         List<BankAccount> bankAccounts = bankAccountService.getBankAccountsByAgent(userId);
         return ResponseEntity.ok(bankAccounts.stream().map(bankAccountMapper::toDto).toList());
+    }
+
+
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create new bank account", description = "bank employee can create new bank account")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = List.class,
+                            subTypes = {BankAccountDto.class}))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
+    public ResponseEntity<Boolean> createBankAccount(@RequestBody CreateBankAccountRequest createBankAccountRequest) {
+        BankAccount bankAccount = bankAccountService.createBankAccount(createBankAccountRequest);
+        if(bankAccount!=null)
+            return ResponseEntity.ok(true);
+        return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
     }
 }
