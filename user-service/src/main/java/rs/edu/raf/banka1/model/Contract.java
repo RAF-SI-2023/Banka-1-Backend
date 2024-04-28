a4b4d16b984cf7284010a110d331d552521f29a6
Andreja Dikic <andreja.dikic@cake.com>
2024-04-28 12:46:40 +0200

    contract

--- user-service/src/main/java/rs/edu/raf/banka1/model/Contract.java
+++ user-service/src/main/java/rs/edu/raf/banka1/model/Contract.java
@@ -1,6 +1,26 @@
 package rs.edu.raf.banka1.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "buyer_id", nullable = false)
     private BankAccount buyer;
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "seller_id", nullable = false)
     private BankAccount seller;
+    private Boolean bankApproval;
+    private Boolean sellerApproval;
+    private String comment;
+    private Long creationDate;
+    private Long realizationDate;
 }
---
+++ user-service/src/main/java/rs/edu/raf/banka1/model/Offer.java
@@ -0,0 +1,5 @@
+package rs.edu.raf.banka1.model;
+
+public class Offer {
+
+}
