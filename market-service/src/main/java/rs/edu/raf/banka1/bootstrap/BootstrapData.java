cdce12b0b84da19ddd8ba1d9644dd1669abb5978
Marko <marko.nikacevic1@gmail.com>
2024-05-06 21:09:40 +0200

    BootstrapData2

--- market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ market-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -1,14 +1,12 @@
 package rs.edu.raf.banka1.bootstrap;
 
-import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.tinylog.Logger;
 @Component
-@AllArgsConstructor
 public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        Logger.info("Loading Data...");
+        Logger.info("All data loaded...");
     }
 }
