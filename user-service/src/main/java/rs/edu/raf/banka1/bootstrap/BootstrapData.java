1d2d32c86d71243e889b9dabc9669c447bd11d50
Mihail Jovanoski <45544573+jovanoski1@users.noreply.github.com>
2024-05-24 16:09:52 +0200

    added api for transfer reports (#285)
    Co-authored-by: jovanoski1 <mihailjovanoski14@email.com>

--- user-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ user-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -13,6 +13,7 @@ import rs.edu.raf.banka1.model.*;
 import rs.edu.raf.banka1.repositories.*;
 import rs.edu.raf.banka1.requests.BankAccountRequest;
 import rs.edu.raf.banka1.requests.CreateBankAccountRequest;
+import rs.edu.raf.banka1.requests.CreateTransferRequest;
 import rs.edu.raf.banka1.services.BankAccountService;
 import rs.edu.raf.banka1.services.CapitalService;
 import rs.edu.raf.banka1.services.EmployeeService;
@@ -245,6 +246,10 @@ public class BootstrapData implements CommandLineRunner {
         bankAccount3.setSubtypeOfAccount("LICNI");
         bankAccountService.saveBankAccount(bankAccount3);
         // dovde
+
+        transferService.processTransfer(transferService.createTransfer(new CreateTransferRequest(bankAccount3.getAccountNumber(), bankAccount2.getAccountNumber(), 100.0)));
+        transferService.processTransfer(transferService.createTransfer(new CreateTransferRequest(bankAccount3.getAccountNumber(), bankAccount1.getAccountNumber(), 100.0)));
+
 
         BankAccountRequest bankAccountRequest = new BankAccountRequest();
         bankAccountRequest.setAccountType(AccountType.FOREIGN_CURRENCY);
