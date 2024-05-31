439b517d11c0705bf0fd504820bf0bb768533161
jovanar28 <jovanar28@gmail.com>
2024-05-31 13:21:04 +0200

    testovi za get metode

--- market-service/src/main/java/rs/edu/raf/banka1/services/OptionsService.java
+++ market-service/src/main/java/rs/edu/raf/banka1/services/OptionsService.java
@@ -1,13 +1,8 @@
 package rs.edu.raf.banka1.services;
 
-<<<<<<< HEAD
-import org.springframework.cache.annotation.Cacheable;
-=======
->>>>>>> 4380c86e99aa5b355c1a87acb9cf2b6f4432149f
 import rs.edu.raf.banka1.model.OptionsModel;
 import rs.edu.raf.banka1.model.dtos.OptionsDto;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,11 +10,8 @@ public interface OptionsService {
     List<OptionsDto> getOptionsByTicker(String ticker);
     Optional<OptionsModel> findById(Long id);
     List<OptionsModel> getAllOptions();
-<<<<<<< HEAD
     Optional<List<OptionsModel>> getAllCallOptions();
     Optional<OptionsModel> getCallOptionById(Long id);
     Optional<List<OptionsModel>> getAllPutOptions();
     Optional<OptionsModel> getPutOptionById(Long id);
-=======
->>>>>>> 4380c86e99aa5b355c1a87acb9cf2b6f4432149f
 }
