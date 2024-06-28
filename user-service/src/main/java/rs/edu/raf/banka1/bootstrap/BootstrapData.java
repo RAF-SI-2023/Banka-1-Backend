b1521bad1747dbded23d4a5a63190c4903ec3491
Mihail Jovanoski <45544573+jovanoski1@users.noreply.github.com>
2024-06-28 20:33:29 +0200

    added otc_trade to user service (#337)
    Co-authored-by: jovanoski1 <mihailjovanoski14@email.com>

--- user-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
+++ user-service/src/main/java/rs/edu/raf/banka1/bootstrap/BootstrapData.java
@@ -12,7 +12,9 @@ import rs.edu.raf.banka1.dtos.market_service.OptionsDto;
 import rs.edu.raf.banka1.mapper.BankAccountMapper;
 import rs.edu.raf.banka1.mapper.CapitalMapper;
 import rs.edu.raf.banka1.model.*;
+import rs.edu.raf.banka1.model.listing.MyStock;
 import rs.edu.raf.banka1.repositories.*;
+import rs.edu.raf.banka1.repositories.otc_trade.MyStockRepository;
 import rs.edu.raf.banka1.requests.BankAccountRequest;
 import rs.edu.raf.banka1.requests.CreateBankAccountRequest;
 import rs.edu.raf.banka1.requests.CreateTransferRequest;
@@ -59,6 +61,7 @@ public class BootstrapData implements CommandLineRunner {
     private final ContractRepository contractRepository;
 
     private final MarginAccountRepository marginAccountRepository;
+    private final MyStockRepository myStockRepository;
 
     private final BankAccountRepository bankAccountRepository;
     private final BankAccountMapper bankAccountMapper;
@@ -89,7 +92,8 @@ public class BootstrapData implements CommandLineRunner {
         final ContractRepository contractRepository,
         final BankAccountRepository bankAccountRepository,
         final BankAccountMapper bankAccountMapper,
-        final CapitalMapper capitalMapper) {
+        final CapitalMapper capitalMapper,
+    MyStockRepository myStockRepository) {
 
         this.employeeRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -110,6 +114,7 @@ public class BootstrapData implements CommandLineRunner {
         this.marginAccountRepository = marginAccountRepository;
         this.transferRepository = transferRepository;
         this.contractRepository = contractRepository;
+        this.myStockRepository = myStockRepository;
         this.bankAccountRepository = bankAccountRepository;
         this.bankAccountMapper = bankAccountMapper;
         this.capitalMapper = capitalMapper;
@@ -117,6 +122,40 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
+        if (myStockRepository.count() == 0) {
+            MyStock stok1 = new MyStock();
+            stok1.setTicker("STK1");
+            stok1.setAmount(100);
+            stok1.setCurrencyMark("RSD");
+            stok1.setPrivateAmount(50);
+            stok1.setPublicAmount(50);
+            stok1.setCompanyId(1L);
+            stok1.setUserId(null);
+            stok1.setMinimumPrice(500.0);
+
+            MyStock stok2 = new MyStock();
+            stok2.setTicker("STK2");
+            stok2.setAmount(100);
+            stok2.setCurrencyMark("RSD");
+            stok2.setPrivateAmount(50);
+            stok2.setPublicAmount(50);
+            stok2.setCompanyId(1L);
+            stok2.setUserId(null);
+            stok2.setMinimumPrice(1500.0);
+
+            MyStock stok3 = new MyStock();
+            stok3.setTicker("STK3");
+            stok3.setAmount(100);
+            stok3.setCurrencyMark("RSD");
+            stok3.setPrivateAmount(50);
+            stok3.setPublicAmount(50);
+            stok3.setCompanyId(1L);
+            stok3.setUserId(null);
+            stok3.setMinimumPrice(200.0);
+
+            myStockRepository.saveAll(List.of(stok1, stok2, stok3));
+        }
 
         if(employeeRepository.findByEmail("admin").isPresent()) {
             return;
